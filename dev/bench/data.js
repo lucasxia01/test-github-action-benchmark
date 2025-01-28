1971b0563b13585c126d40522461d750f1e183f5
github-action-benchmark <github@users.noreply.github.com>
2025-01-28 21:36:02 +0000

    add C++ Benchmark (googlecpp) benchmark result for cc5fc7795ce112cdb2d959a95f61fd35a2be7c7d

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1738100162577,
+  "repoUrl": "https://github.com/lucasxia01/test-github-action-benchmark",
+  "entries": {
+    "C++ Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "lucasxia01@gmail.com",
+            "name": "Lucas Xia",
+            "username": "lucasxia01"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cc5fc7795ce112cdb2d959a95f61fd35a2be7c7d",
+          "message": "add github token",
+          "timestamp": "2025-01-28T15:55:38-05:00",
+          "tree_id": "8338d0c77ce022bfb42be30e803f01b27e06dfdd",
+          "url": "https://github.com/lucasxia01/test-github-action-benchmark/commit/cc5fc7795ce112cdb2d959a95f61fd35a2be7c7d"
+        },
+        "date": 1738100161635,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "nativeClientIVCBench/Ambient_17_in_20/6",
+            "value": 42039.371671155095,
+            "unit": "ms/iter",
+            "extra": "iterations: 1\ncpu: 19569.430315 ms\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
